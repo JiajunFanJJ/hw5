0fed7e433c3c991f44e59496556ceca069d267e6
JiajunFanJJ <55294567+JiajunFanJJ@users.noreply.github.com>
2019-10-12 13:38:07 -0700

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  for (var x = 20; x <= 390; x = x + 20) {
+    for (var y = 20; y <= 390; y = y + 20) {
+      ellipse(x, y, 15);
+    }
+  }
+}
